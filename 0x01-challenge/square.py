da69821a7df8919cfd5824994d24b68dabb18905
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-10-17 06:02:48 -0700

    sq

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+
 class square:
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -16,7 +18,9 @@ class square:
     def __str__(self):
         return "{}/{}".format(self.width, self.width)
 
+
 if __name__ == "__main__":
+
     s = square(width=12)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
